2c715be948fb58644063a3103989610a9c1e55aa
Yogimuhmmadirshad13 <yogimuhammadirshad@gmail.com>
2025-03-28 01:34:05 +0700

    Update Table

--- flowfin/src/components/DataTable.tsx
+++ flowfin/src/components/DataTable.tsx
@@ -1,30 +1,92 @@
+import { Edit2, Trash } from "iconsax-react";
+
 export default function DataTable() {
     return (
         <>
             <table className="table-auto mt-4 w-full text-left">
                 <thead>
-                    <tr className=''>
-                        <th className='bg-gray-100 p-3 font-semibold rounded-tl-lg'>No</th>
-                        <th className='bg-gray-100 p-3 font-semibold'>Product Name</th>
-                        <th className='bg-gray-100 p-3 font-semibold'>Price</th>
-                        <th className='bg-gray-100 p-3 font-semibold'>Description</th>
-                        <th className='bg-gray-100 p-3 font-semibold rounded-tr-lg'>Action</th>
+                    <tr>
+                        <th className='bg-gray-100 p-3 font-semibold rounded-tl-lg text-[14px] text-center'>No</th>
+                        <th className='bg-gray-100 p-3 font-semibold text-center'>Tanggal</th>
+                        <th className='bg-gray-100 p-3 font-semibold text-center'>Kategori Pengeluaran</th>
+                        <th className='bg-gray-100 p-3 font-semibold text-center'>Jumlah Pengeluaran</th>
+                        <th className='bg-gray-100 p-3 font-semibold text-center'>Deskripsi Transaksi</th>
+                        <th className='bg-gray-100 p-3 font-semibold rounded-tr-lg text-center'>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className='border-b-2 border-gray-200'>
-                        <td className='index-info px-3'>test</td>
-                        <td className='product-name py-4 flex items-center gap-4'>
-
-                        </td>
-                        <td className='price-info px-3'>test</td>
-                        <td className='desc-info px-3'>Test</td>
-                        <td className='action px-3'>
-                            <button className='bg-gray-100 p-3 rounded-md cursor-pointer m-3 hover:bg-red-600 hover:text-white'>
-                                Test
+                        <td className='index-info px-3 text-[14px] font-medium'>1</td>
+                        <td className='tanggal px-3 text-[14px] font-medium'>26/03/2025</td>
+                        <td className='kategori-penjualan px-3 text-[14px] font-medium'>Penjualan Video Tutorial</td>
+                        <td className='jumlah px-3 text-[14px] font-medium'>Rp. 350.000</td>
+                        <td className='deskripsi px-3 text-[14px] font-medium'>Penjualan video tutorial membuat aplikasi manajemen kas berbasis web</td>
+                        <td className='aksi'>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-gray-100 hover:text-white'>
+                                <Edit2 size="20" color="#797B8C" variant="Bold" />
                             </button>
-                            <button className='bg-gray-100 p-3 rounded-md cursor-pointer m-3 hover:bg-red-600 hover:text-white'>
-                                Test
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-red-100 hover:text-white'>
+                                <Trash size="20" color="#F74B4B" variant="Bold" />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr className='border-b-2 border-gray-200'>
+                        <td className='index-info px-3 text-[14px] font-medium'>1</td>
+                        <td className='tanggal px-3 text-[14px] font-medium'>26/03/2025</td>
+                        <td className='kategori-penjualan px-3 text-[14px] font-medium'>Penjualan Video Tutorial</td>
+                        <td className='jumlah px-3 text-[14px] font-medium'>Rp. 350.000</td>
+                        <td className='deskripsi px-3 text-[14px] font-medium'>Penjualan video tutorial membuat aplikasi manajemen kas berbasis web</td>
+                        <td className='aksi'>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-gray-100 hover:text-white'>
+                                <Edit2 size="20" color="#797B8C" variant="Bold" />
+                            </button>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-red-100 hover:text-white'>
+                                <Trash size="20" color="#F74B4B" variant="Bold" />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr className='border-b-2 border-gray-200'>
+                        <td className='index-info px-3 text-[14px] font-medium'>1</td>
+                        <td className='tanggal px-3 text-[14px] font-medium'>26/03/2025</td>
+                        <td className='kategori-penjualan px-3 text-[14px] font-medium'>Penjualan Video Tutorial</td>
+                        <td className='jumlah px-3 text-[14px] font-medium'>Rp. 350.000</td>
+                        <td className='deskripsi px-3 text-[14px] font-medium'>Penjualan video tutorial membuat aplikasi manajemen kas berbasis web</td>
+                        <td className='aksi'>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-gray-100 hover:text-white'>
+                                <Edit2 size="20" color="#797B8C" variant="Bold" />
+                            </button>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-red-100 hover:text-white'>
+                                <Trash size="20" color="#F74B4B" variant="Bold" />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr className='border-b-2 border-gray-200'>
+                        <td className='index-info px-3 text-[14px] font-medium'>1</td>
+                        <td className='tanggal px-3 text-[14px] font-medium'>26/03/2025</td>
+                        <td className='kategori-penjualan px-3 text-[14px] font-medium'>Penjualan Video Tutorial</td>
+                        <td className='jumlah px-3 text-[14px] font-medium'>Rp. 350.000</td>
+                        <td className='deskripsi px-3 text-[14px] font-medium'>Penjualan video tutorial membuat aplikasi manajemen kas berbasis web</td>
+                        <td className='aksi'>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-gray-100 hover:text-white'>
+                                <Edit2 size="20" color="#797B8C" variant="Bold" />
+                            </button>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-red-100 hover:text-white'>
+                                <Trash size="20" color="#F74B4B" variant="Bold" />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr className='border-b-2 border-gray-200'>
+                        <td className='index-info px-3 text-[14px] font-medium'>1</td>
+                        <td className='tanggal px-3 text-[14px] font-medium'>26/03/2025</td>
+                        <td className='kategori-penjualan px-3 text-[14px] font-medium'>Penjualan Video Tutorial</td>
+                        <td className='jumlah px-3 text-[14px] font-medium'>Rp. 350.000</td>
+                        <td className='deskripsi px-3 text-[14px] font-medium'>Penjualan video tutorial membuat aplikasi manajemen kas berbasis web</td>
+                        <td className='aksi'>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-gray-100 hover:text-white'>
+                                <Edit2 size="20" color="#797B8C" variant="Bold" />
+                            </button>
+                            <button className='p-3 rounded-md cursor-pointer mx-1.5 my-3 hover:bg-red-100 hover:text-white'>
+                                <Trash size="20" color="#F74B4B" variant="Bold" />
                             </button>
                         </td>
                     </tr>
