a8f57d0d297b641b2b882e8c5d99e4dc546d8bdd
FahmiAlmsyh <alamsyahf100@gmail.com>
2025-03-30 12:39:50 +0700

    fix: dropdown

--- flowfin/src/components/AnimatedDropdown.tsx
+++ flowfin/src/components/AnimatedDropdown.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ReactNode, MouseEventHandler } from "react";
+import React, { ReactNode, MouseEventHandler } from "react";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
@@ -7,10 +7,14 @@ import { ChevronDownIcon } from "@heroicons/react/24/solid";
 export default function AnimatedDropdown({
   label,
   children,
+  selected,
+  onSelect,
 }: {
   label: string;
   children: ReactNode;
+  selected?: string;
   className?: string;
+  onSelect: (value: string) => void;
 }) {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -37,7 +41,7 @@ export default function AnimatedDropdown({
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center cursor-pointer px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        {label}
+        {selected || label}
         <ChevronDownIcon
           className={`ml-2 w-5 h-5 transition-transform duration-200 ${
             isOpen ? "rotate-180" : "rotate-0"
@@ -54,7 +58,20 @@ export default function AnimatedDropdown({
             exit="closed"
             className="absolute z-10 mt-2 w-full bg-white border border-gray-200 rounded-md shadow-lg"
           >
-            <div className="py-1">{children}</div>
+            <div className="py-1">
+              {React.Children.map(children, (child) => {
+                if (React.isValidElement(child)) {
+                  return React.cloneElement(child, {
+                    // @ts-ignore
+                    onSelect: (value: string) => {
+                      onSelect(value);
+                      setIsOpen(false);
+                    },
+                  });
+                }
+                return child;
+              })}
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
@@ -64,14 +81,16 @@ export default function AnimatedDropdown({
 
 export function DropdownItem({
   children,
-  onClick,
+  value,
+  onSelect,
 }: {
   children: ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+  value: string;
+  onSelect: (value: string) => void;
 }) {
   return (
     <button
-      onClick={onClick}
+      onClick={() => onSelect(value)}
       className="block w-full text-left cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
     >
       {children}
